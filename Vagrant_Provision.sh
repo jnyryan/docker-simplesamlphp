37dd65ed6c6bb4f0360ed7bc363314b7bafd9723
John Ryan <jnyryan@gmail.com>
2014-08-27 17:56:24 +0100

    updated provision

--- Vagrant_Provision.sh
+++ Vagrant_Provision.sh
@@ -10,7 +10,7 @@ apt-get install -y git subversion curl htop
 # apache2 server
 apt-get install -y apache2
 apt-get install -y apache2-doc apache2-suexec-pristine apache2-suexec-custom apache2-utils openssl-blacklist
-apt-get install -y php-pear libmcrypt-dev mcrypt php5-user-cache
+apt-get install -y php-pear libmcrypt-dev mcrypt
 apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
 apt-get install -y php5-common php5-cli php5-curl php5-gmp php5-ldap
 apt-get install -y libapache2-mod-gnutls
@@ -30,15 +30,15 @@ openssl req -x509 -batch -nodes -newkey rsa:2048 -keyout /etc/ssl/private/server
 ####################
 # SimpleSaml
 
-git clone https://github.com/simplesamlphp/simplesamlphp.git /var/simplesamlphp
-mkdir -p /var/simplesamlphp/config && cp -r /var/simplesamlphp/config-templates/* /var/simplesamlphp/config/
-mkdir -p /var/simplesamlphp/metadata && cp -r /var/simplesamlphp/metadata-templates/* /var/simplesamlphp/metadata/
+cd /var
+git clone https://github.com/simplesamlphp/simplesamlphp.git
+cd /var/simplesamlphp
+mkdir -p config && cp -r config-templates/* config/
+mkdir -p metadata && cp -r metadata-templates/* metadata/
 cp /vagrant/etc/simplesamlphp/config.php /var/simplesamlphp/config/config.php
+cp /vagrant/etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.conf
 cp /vagrant/etc/simplesamlphp/authsources.php /var/simplesamlphp/config/authsources.php
 cp /vagrant/etc/simplesamlphp/saml20-idp-remote.php /var/simplesamlphp/metadata/saml20-idp-remote.php
-cp /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.conf.bak
-cp /vagrant/etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.conf
-
 
 ####################
 # Composer
